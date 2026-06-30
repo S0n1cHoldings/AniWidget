ac8b451e49cb32ce297c493702d4780d1a7ea9a7
s0n1c <me@s0n1c.ca>
2026-06-30 17:26:24 -0400

    feat: icon and title

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { fade } from "svelte/transition";
 	import "./layout.css";
 	import "framework7-icons/css/framework7-icons.css";
+	import { glyphs } from "../assets";
 
 	let { children } = $props();
 
@@ -11,6 +12,11 @@
 		aniwidget_init($user_token);
 	});
 </script>
+
+<svelte:head>
+	<link rel="icon" href={glyphs.aniwidget} />
+	<title>AniWidget</title>
+</svelte:head>
 
 {#if $mounted}
 	<main in:fade={{ duration: 200 }} class="max-w-130 mx-auto py-8 px-4">
